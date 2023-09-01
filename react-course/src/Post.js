5ca16076a9249db3ceb4254cb47684ecd46417bd
Alquimista-07 <jdca.07@gmail.com>
2023-09-01 11:42:23 -0500

    13. Third Party modules, react-icons

--- react-course/src/Post.js
+++ react-course/src/Post.js
@@ -1,6 +1,21 @@
 // NOTA:Otra forma de crear un un componente con una sintáxis diferente
 //      es usar una función flecha  asignada a una constante.
 
+// Podemos usar bibliotecas de terceros las cuales se instalan con el gestor de node
+// por ejemplo acá vamos a usar react icons el cual se instala: npm install react-icons
+// y se debe importar para poder usarla. Adicionalmente existen muchas bibliotecas como
+// por ejemplo para remplazar fecth y tener una mejor forma de manejar peticiones, 
+// añadir animaciones, bibliotecas para crear formularios, modeles. Un ejemplo de estas 
+// otras bibliotecas por ejemplo es Material UI (MIUI) que su documentación oficial se
+// encuentra en: https://mui.com/
+//
+// NOTA: La web con la documentación oficial de react-icons es: 
+//       https://react-icons.github.io/react-icons/
+//
+// En este caso elegimos los iconos de VS Code pero existen muchos otros por ejemplo de 
+// font-awesom, Material Icons, etc
+import { VscGlobe } from "react-icons/vsc";
+
 export const Post = () => {
     return <button onClick={() => {
         // Esto es una promesa en JS y tenemos que tener en cuenta deemos tener en cuenta
@@ -10,6 +25,8 @@ export const Post = () => {
             .then(data => console.log('Datos:', data ))
             .catch(error => console.error(error));
     }}>
+        {/* Y usamos el icono que elegimos */}
+        <VscGlobe/>
         Traer datos
     </button>
 }
