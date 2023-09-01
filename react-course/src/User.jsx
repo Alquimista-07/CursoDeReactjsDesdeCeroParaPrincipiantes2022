2dee15e7e54bef180777e33466aba847cc0b1825
Alquimista-07 <jdca.07@gmail.com>
2023-09-01 07:03:01 -0500

    6. Extension jsx

---
+++ react-course/src/User.jsx
@@ -0,0 +1,3 @@
+// NOTA: Podemos usar la extensión .js o .jsx en el archivo, pero funcionan de la misma forma.
+//       Algo que si hay que tener en cuenta es que si usamos la herramienta Vitejs si nos va 
+//       a pedir que la extensión debe ser .jsx en cambio create-app no nos va a obligar.
